88ef3d98477e3cb6abb4d62c1e8201f0e68891c4
cor22982 <cor22982@uvg.edu.gt>
2024-02-18 03:17:09 -0600

    Agregar script de python para llenar base de datos

--- Data/llenadobase.py
+++ Data/llenadobase.py
@@ -154,6 +154,155 @@ def creacion_games(cursor, connection, archivo_csv):
                            VALUES ({", ".join(["%s" for _ in range(len(columnas))])})'''
         cursor.executemany(insert_query, datos_a_insertar)
 
+def creacion_appearances(cursor,connection,archivo_csv):
+    create_table_appearances = '''
+    CREATE TABLE appearances (
+        gameID INT,
+        playerID INT,
+        goals INT,
+        ownGoals INT,
+        shots INT,
+        xGoals FLOAT,
+        xGoalsChain FLOAT,
+        xGoalsBuildup FLOAT,
+        assists INT,
+        keyPasses INT,
+        xAssists FLOAT,
+        position VARCHAR(30),
+        positionOrder INT,
+        yellowCard INT,
+        redCard INT,
+        time INT,
+        substituteIn INT,
+        substituteOut INT,
+        leagueID INT,
+        foreign key (gameID) references games(gameID) ,
+        foreign key (leagueID) references leagues(leagueID) ,
+        foreign key (playerID) references players(playerID)
+    );
+    '''
+    
+    cursor.execute(create_table_appearances)
+    columnas = [
+        "gameID", "playerID", "goals", "ownGoals", "shots", "xGoals", "xGoalsChain",
+        "xGoalsBuildup", "assists", "keyPasses", "xAssists", "position", "positionOrder",
+        "yellowCard", "redCard", "time", "substituteIn", "substituteOut", "leagueID"
+    ]
+
+    with open(archivo_csv, 'r', newline='', encoding='utf-8') as archivo:
+        lector_csv = csv.reader(archivo)
+        next(lector_csv)  # Ignorar la primera fila si contiene encabezados
+
+        # Preparar los datos para la inserción
+        datos_a_insertar = []
+        for fila in lector_csv:
+            # Suponiendo que tienes las columnas en el mismo orden que aparecen en el archivo CSV
+            datos_fila = tuple(fila)
+            datos_a_insertar.append(datos_fila)
+
+        # Ejecutar la consulta de inserción para todas las filas
+        insert_query = f'''INSERT INTO appearances ({", ".join(columnas)})
+                           VALUES ({", ".join(["%s" for _ in range(len(columnas))])})'''
+        cursor.executemany(insert_query, datos_a_insertar)
+    print("Creacion de appearances")
+
+def creacion_teamstats(cursor, connection, archivo_csv):
+    create_table_appearances = '''
+    CREATE TABLE teamstats (
+        gameID INT,
+        teamID INT,
+        season INT,
+        date TIMESTAMP,
+        location VARCHAR(1),
+        goals INT,
+        xGoals FLOAT,
+        shots INT,
+        shotsOnTarget INT,
+        deep FLOAT,
+        ppda FLOAT,
+        fouls INT,
+        corners INT,
+        yellowCards INT,
+        redCards INT,
+        result VARCHAR(1),
+        foreign key (gameID) references games(gameID),
+        foreign key (teamID) references teams(teamID)
+    );
+    '''
+    cursor.execute(create_table_appearances)
+    
+    columnas = [
+        "gameID", "teamID", "season", "date", "location", "goals", "xGoals",
+        "shots", "shotsOnTarget", "deep", "ppda", "fouls", "corners",
+        "yellowCards", "redCards", "result"
+    ]
+
+
+    with open(archivo_csv, 'r', newline='', encoding='utf-8') as archivo:
+        lector_csv = csv.reader(archivo)
+        next(lector_csv)  # Ignorar la primera fila si contiene encabezados
+
+        # Preparar los datos para la inserción
+        datos_a_insertar = []
+        for fila in lector_csv:
+            # Convertir la fecha y hora a formato TIMESTAMP
+            fecha_hora_str = fila[3]  # Suponiendo que la fecha y hora están en la cuarta columna (0-indexed)
+            fecha_hora = datetime.strptime(fecha_hora_str, '%m/%d/%Y %H:%M')
+            fila[3] = fecha_hora.strftime('%Y-%m-%d %H:%M:%S')  # Convertir a formato TIMESTAMP
+
+            # Suponiendo que tienes las columnas en el mismo orden que aparecen en el archivo CSV
+            datos_fila = tuple(fila)
+            datos_a_insertar.append(datos_fila)
+
+        # Ejecutar la consulta de inserción para todas las filas
+        insert_query = f'''INSERT INTO teamstats ({", ".join(columnas)})
+                           VALUES ({", ".join(["%s" for _ in range(len(columnas))])})'''
+        cursor.executemany(insert_query, datos_a_insertar)
+    print("Creacion de Teamstats Exitosa")
+
+def creacion_shots(cursor, connection, archivo_csv):
+    create_table_appearances = '''
+    CREATE TABLE shots (
+        gameID INT,
+        shooterID INT,
+        assisterID INT,
+        minute INT,
+        situation VARCHAR(50),
+        lastAction VARCHAR(50),
+        shotType VARCHAR(50),
+        shotResult VARCHAR(50),
+        xGoal FLOAT,
+        positionX FLOAT,
+        positionY FLOAT,
+        foreign key (gameID) references games(gameID),
+        foreign key (shooterID) references players(playerID),
+        foreign key (assisterID) references players(playerID)
+    );
+    '''
+    cursor.execute(create_table_appearances)
+    columnas = [
+        "gameID", "shooterID", "assisterID", "minute", "situation", "lastAction",
+        "shotType", "shotResult", "xGoal", "positionX", "positionY"
+    ]
+    with open(archivo_csv, 'r', newline='', encoding='utf-8') as archivo:
+        lector_csv = csv.reader(archivo)
+        next(lector_csv)  # Ignorar la primera fila si contiene encabezados
+
+        # Preparar los datos para la inserción
+        datos_a_insertar = []
+        for fila in lector_csv:
+            # Suponiendo que tienes las columnas en el mismo orden que aparecen en el archivo CSV
+            gameID, shooterID, assisterID, minute, situation, lastAction, shotType, shotResult, xGoal, positionX, positionY = fila
+            # Convertir 'NA' o vacío a None (NULL en SQL)
+            assisterID = int(assisterID) if assisterID.isdigit() else None
+            datos_fila = (gameID, shooterID, assisterID, minute, situation, lastAction, shotType, shotResult, xGoal, positionX, positionY)
+            datos_a_insertar.append(datos_fila)
+
+        # Ejecutar la consulta de inserción para todas las filas
+        insert_query = f'''INSERT INTO shots ({", ".join(columnas)})
+                           VALUES ({", ".join(["%s" for _ in range(len(columnas))])})'''
+        cursor.executemany(insert_query, datos_a_insertar)
+    print("Creacion de Shots Exitosa")
 
 try:
     connection=psycopg2.connect(
@@ -165,13 +314,15 @@ try:
     
     #CREACION TABLA teams
     cursor = connection.cursor()
-    #creacion_teams(cursor,connection,'./Data/teams.csv')
-    #creacion_leagues(cursor,connection,'./Data/leagues.csv')
-    #creacion_players(cursor,connection,'./Data/players.csv')
-    #connection.commit()
-    creacion_games(cursor,connection,'./Data/games.csv')
+    creacion_teams(cursor,connection,'./Data/teams.csv')
+    creacion_leagues(cursor,connection,'./Data/leagues.csv')
+    creacion_players(cursor,connection,'./Data/players.csv')
     connection.commit()
-    #Ahora vamos a crear los datos. 
+    creacion_games(cursor,connection,'./Data/games.csv')
+    creacion_appearances(cursor,connection,'./Data/appearances.csv')
+    creacion_teamstats(cursor,connection,'./Data/teamstats.csv')
+    creacion_shots(cursor,connection,'./Data/shots.csv')
+    connection.commit()
 
 except Exception as ex:
     print(ex)
